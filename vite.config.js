d5ca8657134702e04fd987f8ee423790ae355cc7
Mikhail Chemezov <michlenanosoft@gmail.com>
2025-04-11 20:36:12 +0500

    added index.html

--- vite.config.js
+++ vite.config.js
@@ -29,6 +29,7 @@ export default defineConfig({
         outDir: '../dist', // Указываем директорию для сборки
         rollupOptions: {
             input: {
+                index: path.resolve(__dirname, './src/index.html'),
                 components: path.resolve(__dirname, './src/components.html'),
                 login: path.resolve(__dirname, './src/login.html'),
                 view: path.resolve(__dirname, './src/view.html'),
