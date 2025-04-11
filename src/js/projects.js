8aabe66e075bf8be6f3d050e0828df54edcc2b04
Mikhail Chemezov <michlenanosoft@gmail.com>
2025-04-12 03:31:59 +0500

    fixed open fancybox when favorite button or file links are clicked.

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,22 +1,35 @@
 import {Fancybox} from "@fancyapps/ui";
 
 export default function projects() {
-    Fancybox.bind('.projects__item', {
-        // Тип контента
-        type: 'ajax',
-        dragToClose: false,
-        on: {
-            done: (fancybox, slide) => {
-                const event = new CustomEvent('fancybox:contentReady', {
-                    detail: {
-                        content: slide.$content, // DOM-элемент, который подгрузился
-                        slide,
-                        fancybox
-                    }
-                });
+    $(document).on('click', '.projects__item', function (e) {
+        // Проверка, не был ли клик по кнопке внутри строки
+        if (e.target.closest('button, a[target="_blank"]')) return;
 
-                document.dispatchEvent(event);
-            }
-        },
+        let src = $(this).data('src');
+
+        // Запускаем Fancybox
+        Fancybox.show([{
+            src: src,
+            // Тип контента
+            type: 'ajax',
+            dragToClose: false,
+            on: {
+                done: (fancybox, slide) => {
+                    const event = new CustomEvent('fancybox:contentReady', {
+                        detail: {
+                            content: slide.$content, // DOM-элемент, который подгрузился
+                            slide,
+                            fancybox
+                        }
+                    });
+
+                    document.dispatchEvent(event);
+                }
+            },
+        }]);
+    });
+
+    $(document).on('click', '.projects__link', function (e) {
+        e.preventDefault();
     });
 }
--- src/js/test.js
+++ src/js/test.js
@@ -1,3 +1,5 @@
-$(document).on('click', '.js-toggle-favorite', function () {
+$(document).on('click', '.js-toggle-favorite', function (e) {
+    e.preventDefault();
+
     $(this).toggleClass('js-toggle-favorite_active');
 });
