3f2bacf74db123fbf04ddaff79c4b21f9d1c64fa
Mikhail Chemezov <michlenanosoft@gmail.com>
2025-04-13 03:54:24 +0500

    added filter active state transition.

--- src/js/test.js
+++ src/js/test.js
@@ -3,3 +3,15 @@ $(document).on('click', '.js-toggle-favorite', function (e) {
 
     $(this).toggleClass('js-toggle-favorite_active');
 });
+
+$(document).on('click', '.filter__item:not(.filter__item_expand):not(.filter__item_collapse)', function (e) {
+    e.preventDefault();
+
+    $(this).toggleClass('filter__item_active');
+});
+
+$(document).on('click', '.filters__reset', function (e) {
+    e.preventDefault();
+
+    $('.filter__item').removeClass('filter__item_active');
+});
