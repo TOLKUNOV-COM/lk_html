6ef2deb524d32da800758b8e2f67f1260d54fdae
chemezov <chemezov@users.noreply.github.com>
2025-04-12 22:54:42 +0000

    deploy: 3f2bacf74db123fbf04ddaff79c4b21f9d1c64fa

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-$(document).on("click",".js-toggle-favorite",function(t){t.preventDefault(),$(this).toggleClass("js-toggle-favorite_active")});
+$(document).on("click",".js-toggle-favorite",function(e){e.preventDefault(),$(this).toggleClass("js-toggle-favorite_active")});$(document).on("click",".filter__item:not(.filter__item_expand):not(.filter__item_collapse)",function(e){e.preventDefault(),$(this).toggleClass("filter__item_active")});$(document).on("click",".filters__reset",function(e){e.preventDefault(),$(".filter__item").removeClass("filter__item_active")});
