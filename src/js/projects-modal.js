9b24354dda697204fcb077196c558c76f717267f
Mikhail Chemezov <michlenanosoft@gmail.com>
2025-05-10 19:07:10 +0500

    projects-modal.js: move set is_open to nextTick

--- src/js/projects-modal.js
+++ src/js/projects-modal.js
@@ -53,7 +53,9 @@ export default function projectsModal() {
                         // window.history.pushState({}, '', originalUrl);
                     }
 
-                    isFancyboxOpen = false;
+                    setTimeout(() => {
+                        isFancyboxOpen = false
+                    }, 1);
                 }
             },
         });
