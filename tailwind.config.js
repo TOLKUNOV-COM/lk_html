81926e4e874c329e285167d082a6322e1e6424b9
Mikhail Chemezov <michlenanosoft@gmail.com>
2025-05-13 05:59:11 +0500

    stat: done top platforms. optimize stat layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
     content: ['./src/**/*.{html,js,pug,ts,jsx,tsx}'],
     safelist: [
         'grid-cols-4',
+        'text-green-500',
+        'text-lime-400',
+        'text-gold',
+        'text-orange-400',
+        'text-orange-500',
     ],
     theme: {
         fontFamily: {
